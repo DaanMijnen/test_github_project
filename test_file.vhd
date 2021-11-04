5ede8a2131059d2e09f604bd69bd7f23718c4909
DaanMijnen <66091968+DaanMijnen@users.noreply.github.com>
2021-11-04 20:58:49 +0100

    git repository aangemaakt

---
+++ test_file.vhd
@@ -0,0 +1,23 @@
+library ieee; 
+use ieee.std_logic.all;
+
+entity test is 
+	port(
+		A: in std_logic; 
+		B: in std_logic; 
+		C: in std_logic; 
+		Y: out std_logic;
+		X: out std_logic
+		)
+end entity; 
+
+architecture rtl of test is
+
+begin 
+	
+	Y <= A nand B or C; 
+	O <= A and B and C; 
+
+end architecture; 
+
+
